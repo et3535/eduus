89de41d5bd9752e41630ef5eca296d664fbfce8c
et3535 <vet3535v@gmail.com>
2018-07-29 10:40:37 +0900

    20180728 카테고리 , 하위카테고리 추가, url 파싱 개발 진행중

--- client/enroll.js
+++ client/enroll.js
@@ -1,8 +1,42 @@
 Template.enroll.events({
     'click button[name=urlsave]'(evt,tmpl){
         var url = tmpl.find("input[name=urlinfo]").value;
-        alert(tmpl.find("input[name=urlinfo]").value);
-        var Socket = new WebSocket("127.0.0.1", [protocal] );
+        //alert(tmpl.find("input[name=urlinfo]").value);
+        //window.location.href="http://localhost:8080/";
+        //C:\Program Files (x86)\Google\Chrome\Application>chrome.exe --user-data-dir="C:\Program Files (x86)\Google\Chrome\Application\chrome dev session" --disable-web-security
+        //var temp = document.domain;
+        //document.domain = "youtube.com";
+        $.get(url, function(response) {
+            console.log(response);
+        });
+        //document.domain = temp;
+        var obj = {
+          'code':"111"
+          ,"upCode":"222"
+        };
+        $.ajax({
+            type       : "post",
+            contentType: 'application/json',
+            data : JSON.stringify(obj),
+            dataType   : 'json',
+            url        : "http://127.0.0.1:8080/greeting2",
+            error      : function(json){
+                console.log('저장중 오류가 발생하였습니다');
+            },
+            success    : function(data){
+                console.log(data+JSON.stringify(data));
+            }
+        });
+
+/*        $.get("http://127.0.0.1:8080/greeting",function(data,status) {
+            alert("data :"+data+"status:"+status+"JSONStringify:"+JSON.stringify(data));
+        });*/
+/*        $.post("http://127.0.0.1:8080/greeting2",obj,contentType: 'application/json').done(function(data){
+            alert(data+JSON.stringify(data));
+        });*/
+/*        $.post("http://127.0.0.1:8080/greeting2",data:obj,function(data,status){
+            alert("data :"+data+"status:"+status+"JSONStringify:"+JSON.stringify(data));
+        });*/
     }
 });
 
